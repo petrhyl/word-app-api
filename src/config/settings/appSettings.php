7ebb339127b1131128acc1859dee2ffcac8afc9f
Petr Hyl <kykil@seznam.cz>
2025-07-12 13:25:52 +0200

    fix auth links in settings

--- src/config/settings/appSettings.php
+++ src/config/settings/appSettings.php
@@ -5,9 +5,9 @@ $otherSettingsPath = __DIR__ . '/.otherSettings.php';
 $confData = [
     "auth" => [
         "senderType" => "email",
-        "verificationLink" => "http://localhost:3001/word-app/signup/verification/",
+        "verificationLink" => "http://localhost:3001/word-app/signup/verification",
         "loginLink" => "http://localhost:3001/word-app/login",
-        "resetLink" => "http://localhost:3001/word-app/reset-password/",
+        "resetLink" => "http://localhost:3001/word-app/reset-password",
         "useNoReply" => true
     ]
 ];
